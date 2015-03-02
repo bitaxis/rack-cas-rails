924f4afb80005f97e91a94c2038ea146f79f5868
Nathan Brazil <nb@bitaxis.com>
2015-03-01 20:42:23 -0800

    1. Use dotenv gem to shim in my CAS server URL. 2. Test things out with the root_url by adding a root directive to config/routes.rb.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
+# Need to use .env to shim in the CAS server URL so that I don't have to hard-code it into the source code and expose my
+# private URL.  This means I need to have a .env file at the root directory of the gem.  I got this from
+# https://github.com/bkeepers/dotenv#sinatra-or-plain-ol-ruby
+require "dotenv"
+Dotenv.load
+
 require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
